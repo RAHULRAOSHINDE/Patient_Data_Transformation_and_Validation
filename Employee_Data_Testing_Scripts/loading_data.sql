3905af56de1d8a25fed6c1b85f2bf8f12b21101c
USER <1cdb499d-0870-47d0-bc4f-7fe14b379648@infosys.com>
2023-11-22 06:56:40 +0000

    Added commands to load data from local filesystem to tables

---
+++ Employee_Data_Testing_Scripts/loading_data.sql
@@ -0,0 +1,8 @@
+#load data from employee.txt into employee_initial table
+
+LOAD DATA LOCAL INPATH '/root/Desktop/save_folder/Data_Validation_Exercises/employee.txt' INTO TABLE employee_initial;
+
+#load data from employee.txt into employee_initial table
+
+LOAD DATA LOCAL INPATH '/root/Desktop/save_folder/Data_Validation_Exercises/employee_target.txt' INTO TABLE employee_target;
+
