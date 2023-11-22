6d21a6f720ecda380a354c6946c048e6c85954d6
USER <1cdb499d-0870-47d0-bc4f-7fe14b379648@infosys.com>
2023-11-22 06:43:09 +0000

    Added sql query to verify the number of rows in employee_initial and employee_target tables

---
+++ Employee_Data_Testing_Scripts/count_testing.sql
@@ -0,0 +1,9 @@
+#count testing to verify the number of rows in employee_initial and employee_target.
+
+SELECT COUNT(eid) FROM employee_initial;
+
+SELECT COUNT(eid) FROM employee_target;
+
+#The number of rows in employee_initial is 6 and in employee_target is 3.
+
+
