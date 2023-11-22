6861b93f90f7a407fa7ef2adf796350d57c8707c
USER <1cdb499d-0870-47d0-bc4f-7fe14b379648@infosys.com>
2023-11-22 07:00:03 +0000

    Added shell script to findout the number of records between employee_initial and employee_target tables

---
+++ Employee_Data_Testing_Scripts/number_of_records.sql
@@ -0,0 +1,5 @@
+#test case to findout number of records using Unix commands between employee_initial.txt and employee_target.txt files
+
+cat /root/Desktop/save_folder/Data_Validation_Exercises/employee_initial.txt | wc -l
+
+cat /root/Desktop/save_folder/Data_Validation_Exercises/employee_target.txt | wc -l
