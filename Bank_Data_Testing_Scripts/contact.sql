7ed953387819ed59042bc67c532d27ce25ed8f4a
USER <1cdb499d-0870-47d0-bc4f-7fe14b379648@infosys.com>
2023-11-24 05:07:41 +0000

    Implemented queries toreplace month with month number and replace contact which is not specified with unknown

--- Bank_Data_Testing_Scripts/contact.sql
+++ Bank_Data_Testing_Scripts/contact.sql
@@ -1,3 +1,9 @@
+
+#no of rows in contact table =13572
+#no of rows in temp table = 4258
+# hive -e "select * from contact_view where contact = 'unknown' > ~/unknown_contact.txt
+
+
 #Create a table which consists data for contact but without header row
 CREATE EXTERNAL TABLE IF NOT EXISTS contact_source (
 custId int,
@@ -18,7 +24,7 @@ previous_connects int
 ROW FORMAT DELIMITED 
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
-TBLPROPERTIES("creator"="raju")
+TBLPROPERTIES("skip.header.line.count"="1")
 ;
 
 #Load data into contact_source table
@@ -26,15 +32,16 @@ TBLPROPERTIES("creator"="raju")
 LOAD DATA LOCAL INPATH '/root/Desktop/save_folder/Bank_Data_Testing_Scripts/contact.csv' INTO TABLE contact_source;
 
 #Remove duplicate information
-#Where contact is not specified, replace it with “unknown”
+#Where contact is not specified, replace it with unknown
 #Replace the month with month number
 
-CREATE TEMPORARY TABLE temp_tbl_contact AS 
-SELECT DISTINCT custid,
+INSERT OVERWRITE TABLE contact_source 
+SELECT DISTINCT custid,age,job,martial,education,default,balance,
 CASE 
   WHEN contact NOT IN ('unknown','telephone','cellular')
   THEN 'unknown' ELSE contact
   END AS contact,
+  day,
 CASE 
   WHEN month = 'jan' THEN 01
   WHEN month = 'feb' THEN 02
@@ -49,7 +56,23 @@ CASE
   WHEN month = 'nov' THEN 11
   WHEN month = 'dec' THEN 12
 ELSE NULL
-END AS month
+END AS month,
+duration,
+campaign,
+pdays,
+previous_connects
 FROM contact_source;
-TBLPROPERTIES("skip.header.line.count"="1")
 ;
+
+#
+
+
+
+
+
+
+
+
+
+
+
