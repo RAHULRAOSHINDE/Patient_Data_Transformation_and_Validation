ad397b5e1388379e668535a72ef6546317157398
root <root@ip-10-0-34-190.ap-southeast-1.compute.internal>
2024-04-19 08:49:54 +0000

    Added script to count records

---
+++ count_number_of_rows.sh
@@ -0,0 +1,7 @@
+#unix command to perform count testing to verify the number of rows in source and target tables. 
+
+cat /root/Desktop/user_repo/To_Participant/AdditionalSuppliedFiles/patient.txt | wc -l
+
+cat /root/Desktop/user_repo/To_Participant/AdditionalSuppliedFiles/patient_target.txt | wc -l
+
+
